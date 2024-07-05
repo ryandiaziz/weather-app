07d9df86dd35722776dcb9ed9968d094bc7bef28
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2024-07-05 12:47:23 +0700

    add

--- src/main/java/org/example/models/Response.java
+++ src/main/java/org/example/models/Response.java
@@ -4,11 +4,6 @@ public class Response<T> {
     public MetaData metaData;
     public T data;
 
-    public Response(MetaData metaData, T data){
-        this.metaData = metaData;
-        this.data = data;
-    }
-
     public  Response(int status, String message, T data){
         this.metaData = new MetaData(status,message);
         this.data = data;
